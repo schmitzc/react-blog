d0a120ddce07fc96a59abbf3fda662f0b9a907b4
Cassie Koomjian <schmitzc@gmail.com>
2016-06-24 16:56:00 -0700

    Redirect to posts index after creating post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   addErrorClassToField(field) {
     return field.touched && field.invalid ? 'has-danger' : '';
+  }
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // Blog post has been created
+        // Navigate user to posts index
+        this.context.router.push('/');
+      });
   }
 
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
 
         <div className={`form-group ${this.addErrorClassToField(title)}`}>
