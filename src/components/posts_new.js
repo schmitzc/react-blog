e65453c5c50f61b60cad33aa29fd15aaaa3b2476
Cassie Koomjian <schmitzc@gmail.com>
2016-06-24 13:43:37 -0700

    Add form to create new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create Post</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a New Post</h3>
+
+        <div className='form-group'>
+          <label>Title</label>
+          <input type='text' className='form-control' {...title} />
+        </div>
+
+        <div className='form-group'>
+          <label>Categories</label>
+          <input type='text' className='form-control' {...categories} />
+        </div>
+
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea className='form-control' {...content} />
+        </div>
+
+        <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import PostsReducers from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducers
+  posts: PostsReducers,
+  form: formReducer
 });
 
 export default rootReducer;
